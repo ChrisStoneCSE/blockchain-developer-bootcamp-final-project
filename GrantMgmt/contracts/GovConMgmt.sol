36d69ef0f0c35ae96a3f25a9a428bc3db893f46f
Chris Stone <56139153+ChrisStoneCSE@users.noreply.github.com>
2021-10-27 17:09:40 -0600

    Version 5
    Added start and stop dates, reverted to 18 decimals

--- GrantMgmt/contracts/GovConMgmt.sol
+++ GrantMgmt/contracts/GovConMgmt.sol
@@ -9,6 +9,8 @@ import "@openzeppelin/contracts/security/Pausable.sol";
 /*
     Run on Ropsten
     Load up the MetaMask account at: https://faucet.ropsten.be/
+    or https://faucet.metamask.io/
+    or https://faucet.dimensions.network
 */
 
 contract GrantManager is ERC20, ERC20Burnable, Ownable, Pausable {
@@ -21,6 +23,8 @@ contract GrantManager is ERC20, ERC20Burnable, Ownable, Pausable {
         string cageCode;
         string naicsCode;
         string grantNumber;
+        string effectiveDate;
+        string completionDate;
         uint laborCLINValue;
         uint travelCLINValue;
         uint odcValue;
@@ -31,7 +35,7 @@ contract GrantManager is ERC20, ERC20Burnable, Ownable, Pausable {
     // An array of 'AwardInfo' structs
     AwardInfo[] public awards;
 
-    constructor() ERC20("NSFTokens", "NSFD") {
+    constructor() ERC20("NSFXTokens", "NSFX") {
 
     }
 
@@ -43,14 +47,17 @@ contract GrantManager is ERC20, ERC20Burnable, Ownable, Pausable {
         _unpause();
     }
     
+    /*
+    // Let's keep it 18 decimals for now, and round it for display in javascript
     
     function decimals() public view virtual override returns (uint8) {
       return 2;
     }
-
+    */
 
     function mint(address to, uint256 amount) public onlyOwner {
-        _mint(to, amount * 10 ** decimals());
+        // _mint(to, amount * 10 ** decimals());
+        _mint(to, amount);
     }
 
     function _beforeTokenTransfer(address from, address to, uint256 amount)
@@ -66,6 +73,8 @@ contract GrantManager is ERC20, ERC20Burnable, Ownable, Pausable {
             string memory _cageCode, 
             string memory _naicsCode,
             string memory _grantNumber,
+            string memory _effectiveDate,
+            string memory _completionDate,
             uint  _laborCLINValue,
             uint  _travelCLINValue,
             uint  _odcValue,
@@ -77,6 +86,8 @@ contract GrantManager is ERC20, ERC20Burnable, Ownable, Pausable {
             _cageCode,
             _naicsCode,
             _grantNumber,
+            _effectiveDate,
+            _completionDate,
             _laborCLINValue,
             _travelCLINValue,
             _odcValue,
