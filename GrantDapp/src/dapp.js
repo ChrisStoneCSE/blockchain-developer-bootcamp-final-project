a6723992477988e1bf49ba1a2a1ddfcc2780a2bd
Chris Stone <56139153+ChrisStoneCSE@users.noreply.github.com>
2021-11-03 17:23:36 -0600

    Version 12

--- GrantDapp/src/dapp.js
+++ GrantDapp/src/dapp.js
@@ -674,13 +674,12 @@ if (document.getElementById('ss-get-value')) {
 	const ssInputValue = document.getElementById('ss-input-box').value;
 	console.log(ssInputValue)
 
-	var web3 = new Web3(window.ethereum)
+	// var web3 = new Web3(window.ethereum)
 
 	// instantiate smart contract instance
 	
-	const grantMgmt = new web3.eth.Contract(ssABI, ssAddress)
-	grantMgmt.setProvider(window.ethereum)
-
+	// const grantMgmt = new web3.eth.Contract(ssABI, ssAddress)
+	// grantMgmt.setProvider(window.ethereum)
 	await grantMgmt.methods.transferOwnership(ssInputValue).send({from: ethereum.selectedAddress})
 	}
 }
@@ -689,13 +688,10 @@ if (document.getElementById('ss-get-value')) {
 if (document.getElementById('ss-get-value')) {	
 	const ssGetValue = document.getElementById('ss-get-value')
 	ssGetValue.onclick = async () => {
-	var web3 = new Web3(window.ethereum)
-	const grantMgmtStorage = new web3.eth.Contract(ssABI, ssAddress)
-	grantMgmtStorage.setProvider(window.ethereum)
-	var value = await grantMgmtStorage.methods.owner().call()
-	console.log(value)
-	const ssDisplayValue = document.getElementById('ss-display-value')
-	ssDisplayValue.innerHTML = 'Current Contract Owner: ' + value
+		var value = await grantMgmtStorage.methods.owner().call()
+		console.log(value)
+		const ssDisplayValue = document.getElementById('ss-display-value')
+		ssDisplayValue.innerHTML = 'Current Contract Owner: ' + value
 	}
 }
 
@@ -713,48 +709,88 @@ if (document.getElementById('ss-get-value')) {
 if (document.getElementById('pauseButton')) {	
 	const clickPauseButton = document.getElementById('pauseButton')
 	clickPauseButton.onclick = async () => {
-		var web3 = new Web3(window.ethereum)
-		const grantMgmt = new web3.eth.Contract(ssABI, ssAddress)
-		grantMgmt.setProvider(window.ethereum)
 		var value = await grantMgmt.methods.pause().send({from: ethereum.selectedAddress})
-		console.log(value)
+		// console.log(value)
 		var pauseValue = document.getElementById('pause-value');
-		pauseValue.innerHTML = 'True'
+		pauseValue.innerHTML = 'Contract is Paused'
+		const getCheckPauseValue = document.getElementById('checkPauseButton')
+		getCheckPauseValue.style.visibility = "hidden"
+		const getPauseValue = document.getElementById('pauseButton')
+		getPauseValue.style.visibility = "hidden"
+		const getUnPauseValue = document.getElementById('unpauseButton')
+		getUnPauseValue.style.visibility = "visible"
 	}
 }
 // Unpause Button
 if (document.getElementById('unpauseButton')) {
 	const clickUnpauseButton = document.getElementById('unpauseButton')
 	clickUnpauseButton.onclick = async () => {
-		var web3 = new Web3(window.ethereum)
-		const grantMgmt = new web3.eth.Contract(ssABI, ssAddress)
-		grantMgmt.setProvider(window.ethereum)
 		var value = await grantMgmt.methods.unpause().send({from: ethereum.selectedAddress})
-		console.log(value)
+		// console.log(value)
 		var pauseValue = document.getElementById('pause-value');
-		pauseValue.innerHTML = 'False'
+		pauseValue.innerHTML = 'Contract is Not Paused'
+		const getCheckPauseValue = document.getElementById('checkPauseButton')
+		getCheckPauseValue.style.visibility = "hidden"
+		const getUnPauseValue = document.getElementById('unpauseButton')
+		getUnPauseValue.style.visibility = "hidden"
+		const getPauseValue = document.getElementById('pauseButton')
+		getPauseValue.style.visibility = "visible"
 	}
 }
 // Get Paused Status
 if (document.getElementById('checkPauseButton')) {
-	const getPauseValue = document.getElementById('checkPauseButton')
-	getPauseValue.onclick = async () => {
-		var web3 = new Web3(window.ethereum)
-		const grantMgmt = new web3.eth.Contract(ssABI, ssAddress)
-		grantMgmt.setProvider(window.ethereum)
+	const getCheckPauseValue = document.getElementById('checkPauseButton')
+	const getPauseValue = document.getElementById('pauseButton')
+	const getUnPauseValue = document.getElementById('unpauseButton')
+	
+	getPauseValue.style.visibility = "hidden"
+	getUnPauseValue.style.visibility = "hidden"
+
+	getCheckPauseValue.onclick = async () => {
 		var value = await grantMgmt.methods.paused().call()
 		console.log(value)
 		const pausedValue = document.getElementById('pause-value')
-		pausedValue.innerHTML = 'Contract is Paused: ' + value
+		// pausedValue.innerHTML = 'Contract is Paused ' + value
+	
+		if (value == 'false') {
+			pausedValue.innerHTML = 'Contract is Paused'
+			console.log('show pause button')
+			getPauseValue.style.visibility = "hidden"
+			getUnPauseValue.style.visibility = "visible"
+		} else {
+			pausedValue.innerHTML = 'Contract is Not Paused'
+
+			console.log('show unpause button')
+			getUnPauseValue.style.visibility = "hidden"
+			getPauseValue.style.visibility = "visible"
+		}
 	}
 }
+
+
+// Ownership Functionality
+if (document.getElementById('clickTransfer')) {
+	const clickTransfer = document.getElementById('clickTransfer')
+
+	clickTransfer.onclick = async () => {
+		const newOwnerValue = document.getElementById('_newOwner').value;
+		const ownershipChanged = document.getElementById('ownershipChanged')
+		ownershipChanged.innerHTML = ``
+		console.log(newOwnerValue)
+		await grantMgmt.methods.transferOwnership(newOwnerValue).send({from: ethereum.selectedAddress}).then(result => {
+			//console.log(result.transactionHash)
+			ownershipChanged.innerHTML = `New owner is assigned: `
+			ownershipChanged.href = `https://ropsten.etherscan.io/tx/${result.transactionHash}`
+		})
+	}
+}
+
+
+
 // Get Token Information
 if (document.getElementById('checkTokenButton')) {
 	const getTokenSupply = document.getElementById('checkTokenButton')
 	getTokenSupply.onclick = async () => {
-	// var web3 = new Web3(window.ethereum)
-	// const grantMgmt = new web3.eth.Contract(ssABI, ssAddress)
-	// grantMgmt.setProvider(window.ethereum)
 	var valueName = await grantMgmt.methods.name().call()
 	var valueSymbol = await grantMgmt.methods.symbol().call()
 	var valueSupply = await grantMgmt.methods.totalSupply().call()
@@ -773,13 +809,14 @@ if (document.getElementById('checkTokenButton')) {
 if (document.getElementById('mintTokens')) {
 	const clickMintButton = document.getElementById('mintTokens')
 	clickMintButton.onclick = async () => {
+		const mintStatus = document.getElementById('mintingComplete')
+		mintStatus.innerHTML = ``
 		const addressValue = document.getElementById('_receiveAddr').value;
 		console.log(addressValue)
 		const numberTokens = document.getElementById('_numToMint').value;
 		console.log(numberTokens)
 		await grantMgmt.methods.mint(addressValue, numberTokens).send({from: ethereum.selectedAddress}).then(result => {
 			//console.log(result.transactionHash)
-			const mintStatus = document.getElementById('mintingComplete')
 			mintStatus.innerHTML = `Go to here`
 			mintStatus.href = `https://ropsten.etherscan.io/tx/${result.transactionHash}`
 		})
@@ -789,11 +826,9 @@ if (document.getElementById('mintTokens')) {
 if (document.getElementById('checkAwards')) {
 	const getTokenSupply = document.getElementById('checkAwards')
 	getTokenSupply.onclick = async () => {
-	var web3 = new Web3(window.ethereum)
-	const grantMgmt = new web3.eth.Contract(ssABI, ssAddress)
-	grantMgmt.setProvider(window.ethereum)
+
 	var valueAwardsNumber = await grantMgmt.methods.getNumberAwards().call()
-	console.log('Number of Awards: ' + valueAwardsNumber)
+	// console.log('Number of Awards: ' + valueAwardsNumber)
 	const awardsNumberValue = document.getElementById('awardsNumber')
 	awardsNumberValue.innerHTML = 'Number of Awards: ' + valueAwardsNumber
 	} 
@@ -805,15 +840,14 @@ if (document.getElementById('checkAwards')) {
 if (document.getElementById('_awardID')) {
 	const getAwardInfo = document.getElementById('getAwardInfo')
 	getAwardInfo.onclick = async () => {
-		var web3 = new Web3(window.ethereum)
-		const grantMgmt = new web3.eth.Contract(ssABI, ssAddress)
-		grantMgmt.setProvider(window.ethereum)
+	
 		const awardIDValue = document.getElementById('_awardID').value;
 		var valueAwardInfo = await grantMgmt.methods.awards(awardIDValue).call()
 		Object.values(valueAwardInfo).forEach(val => console.log(val));
 		const awardsInfoValue = document.getElementById('awardInfo')
 		let arr = valueAwardInfo;
 		document.getElementById("awardInfo").innerHTML = JSON.stringify(arr, null, 2);
+		awardsNumberValue.innerHTML = 'Number of Awards: ' + valueAwardsNumber
 	}
 } else {
 	// console.log('Get Awards Info Button does not exist.')
